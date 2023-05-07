675603ad6b32946091e57362c84a18a76f26a21f
kevin Medina <kevinnicolasmr@gmail.com>
2023-05-07 20:47:08 -0300

    Detalles del boton

---
+++ assets/js/plugins/button.js
@@ -0,0 +1,25 @@
+console.log('guncionadn')
+
+
+const botonOcultar = document.getElementById('botonOcultar');
+const botonMostrar = document.getElementById('botonMostrar');
+const columna = document.querySelector('.divNew');
+const newDiv22 = document.getElementById('buttonDivNew2');
+
+
+botonOcultar.addEventListener('click', () => {
+    columna.classList.add('hiddenSeccionDiv');
+    newDiv22.classList.add('divNew2WithFull');
+    botonOcultar.classList.add('hiddenSeccionDiv');
+    botonMostrar.classList.add('buttonOnJS');
+    console.log('boton de ocultar clickeado');
+  });
+  
+  botonMostrar.addEventListener('click',()=>{
+    columna.classList.remove('hiddenSeccionDiv');
+    newDiv22.classList.remove('divNew2WithFull');
+    botonOcultar.classList.remove('hiddenSeccionDiv');
+    botonMostrar.classList.remove('buttonOnJS');
+    console.log('boton de prendido clickeado')
+  })
+  
