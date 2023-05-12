d1181517ae6c75678148c3866de0e20cad291fee
kevin Medina <kevinnicolasmr@gmail.com>
2023-05-12 10:45:41 -0300

    agregando el menu mobile

--- assets/js/plugins/button.js
+++ assets/js/plugins/button.js
@@ -1,10 +1,11 @@
-console.log('guncionadn')
+console.log('El archivo esta enlacado')
 
 
 const botonOcultar = document.getElementById('botonOcultar');
 const botonMostrar = document.getElementById('botonMostrar');
 const columna = document.querySelector('.divNew');
 const newDiv22 = document.getElementById('buttonDivNew2');
+
 
 
 botonOcultar.addEventListener('click', () => {
@@ -22,4 +23,18 @@ botonOcultar.addEventListener('click', () => {
     botonMostrar.classList.remove('buttonOnJS');
     console.log('boton de prendido clickeado')
   })
-  
+
+/* Button On Mobile Head AboutMe */ 
+
+const faSliders = document.querySelector(".fa-sliders");
+const menuMobileOptions = document.querySelector(".menuMobileOptions")
+
+faSliders.addEventListener('click',()=>{
+  if(menuMobileOptions.classList.contains("menuMobileOn")){
+    menuMobileOptions.classList.remove("menuMobileOn")
+    
+  }else{
+    menuMobileOptions.classList.add("menuMobileOn")
+  }
+})
+
